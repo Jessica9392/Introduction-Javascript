4b151a1bb0fb5eab5c3ac7078c2cb2e7eb2abe2f
Jessica <737388774@qq.com>
2018-04-30 23:39:08 +0800

    Day1

---
+++ 20180430day1/codecademyP2-.js
@@ -0,0 +1,66 @@
+///2 Console
+console.log("Hello!Pangzi.");                               //=Hello!Pangzi.
+console.log("My favorite pizza is highfun durian pizza");
+console.log("No favorite book, no favorite movie.");
+
+
+///3 Data Types
+console.log('JavaScript');   //strings     
+console.log(33.7);           //numbers
+console.log(true);           //booleans
+console.log(null);           //null
+
+
+///4 Math Operators
+console.log(3.5 + 25);       //add             //=28.5
+console.log(2018 - 1969);    //subtract        //=49
+console.log(65 / 240);       //multiply        //=0.2708333
+console.log(0.2708 * 100);   //divide          //=27.08
+
+
+///5 Properties
+console.log("Teaching the world how to code".length);   //.length  //=30
+
+
+///6 Built in Method
+//JavaScropt Documentation   https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/String/prototype
+// Log Codecademy in all uppercase letters
+console.log('Codecademy'.toUpperCase());        //=CODECADEMY
+// Use a string method to log the following statment without whitespace at the beginning and end of it.
+console.log('    Remove whitespace   '.trim()); //= Remove whitespace
+
+
+///7 Libraries
+// JavaScript math library   https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+console.log(Math.floor(Math.random()*100));   //=8
+console.log(Math.ceil(43.8));                 //=44
+console.log(Number.isInteger(2017));          //=true
+
+
+///8 Comments
+//"//" A single line comment, "/*" and "*/" A multi-line comment;
+//opening line
+console.log('It was love at first sight.');
+
+console.log('The first time Yossarian saw the chaplain he fell madly in love with him.');
+console.log('Yossarian was in the hospital with a pain in his liver that fell just short of being jaundice.');
+console.log('The doctors were puzzled by the fact that it wasn\'t quite jaundice.');
+console.log('If it became jaundice they could treat it.');
+console.log('If it didn\'t become jaundice and went away they could discharge him.');
+console.log('But this just being short of jaundice all the time confused them.');
+/*
+miaomiaomiao;
+wangwangwang;
+zhizhizhi;
+*/
+
+///9 Review Types and Operators
+/*
+Four essential data types in JavaScript include strings, numbers, booleans, and null.
+Data is printed, or logged, to the console with console.log().
+Four built-in mathematical operators include +, -, *, and /.
+JavaScript associates certain properties with different data types.
+JavaScript has built-in methods for different data types.
+Libraries are collections of methods that can be called without an instance.
+You can write single-line comments with // and multi-line comments between /* and */.
+*/
